133e58533cb4f0b813e6b914578c6566758ecc6f
igor-lourenco <igor.lourencosantos@gmail.com>
2020-09-03 15:46:15 -0300

    Datas com Instant e padrao ISO 8601

---
+++ src/main/java/com/example/curso/servicos/PedidoServico.java
@@ -0,0 +1,26 @@
+package com.example.curso.servicos;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.curso.entidades.Pedido;
+import com.example.curso.repositories.PedidoRepository;
+
+@Service
+public class PedidoServico {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public List<Pedido> findAll(){
+		return repository.findAll();
+	}
+	
+	public Pedido findById(Long id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.get();
+	}
+}
