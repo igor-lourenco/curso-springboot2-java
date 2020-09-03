4b0750172b967939e86bb11b1d41c851e584096c
igor-lourenco <igor.lourencosantos@gmail.com>
2020-09-03 16:59:10 -0300

    Produto

---
+++ src/main/java/com/example/curso/servicos/ProdutoServico.java
@@ -0,0 +1,26 @@
+package com.example.curso.servicos;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.curso.entidades.Produto;
+import com.example.curso.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoServico {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	public List<Produto> findAll(){
+		return repository.findAll();
+	}
+	
+	public Produto findById(Long id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.get();
+	}
+}
