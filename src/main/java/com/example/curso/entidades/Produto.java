34ca0c6aadd62097e15ca982cd34acea304e87e1
igor-lourenco <igor.lourencosantos@gmail.com>
2020-09-17 13:10:30 -0300

    associacao Produto com ItemPedido

--- src/main/java/com/example/curso/entidades/Produto.java
+++ src/main/java/com/example/curso/entidades/Produto.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_produto")
@@ -31,6 +34,9 @@ public class Produto implements Serializable{
 	joinColumns = @JoinColumn (name = "produto_id"),
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 
@@ -89,6 +95,16 @@ public class Produto implements Serializable{
 		return categorias;
 	}
 
+	@JsonIgnore
+	public Set<Pedido> getPedidos(){
+		Set<Pedido> set = new HashSet<>();
+		for(ItemPedido x : itens) {
+			set.add(x.getPedido());
+		}
+		
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
