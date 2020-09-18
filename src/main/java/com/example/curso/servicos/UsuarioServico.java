65fc3425d354bf751bf11508c748828150a0b649
igor-lourenco <igor.lourencosantos@gmail.com>
2020-09-18 19:11:36 -0300

    Tratamento Exception handling Update

--- src/main/java/com/example/curso/servicos/UsuarioServico.java
+++ src/main/java/com/example/curso/servicos/UsuarioServico.java
@@ -3,6 +3,8 @@ package com.example.curso.servicos;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,40 +20,44 @@ public class UsuarioServico {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
-	public List<Usuario> findAll(){
+
+	public List<Usuario> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Usuario findById(Long id) {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Usuario inserir(Usuario obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void deletar(Long id) {
 		try {
-		repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public Usuario atualizar(Long id, Usuario obj) {
-		Usuario entidade = repository.getOne(id);
-		atualizarDados(entidade, obj);
-		return repository.save(entidade);
+		try {
+			Usuario entidade = repository.getOne(id);
+			atualizarDados(entidade, obj);
+			return repository.save(obj);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void atualizarDados(Usuario entidade, Usuario obj) {
 		entidade.setNome(obj.getNome());
 		entidade.setEmail(obj.getEmail());
 		entidade.setFone(obj.getFone());
-		
+
 	}
 }
